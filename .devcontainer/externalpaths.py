fb1f784f17558fb97efb1b9b2e27c6aafefb922d
Patrick Irving <psirving@gmail.com>
2025-02-13 22:20:52 +0000

    chore: create dev_container

---
+++ .devcontainer/externalpaths.py
@@ -0,0 +1,13 @@
+ringmapperpath = '/usr/local/RingMapper/'
+structureanalysistoolspath = '/usr/local/StructureAnalysisTools/'
+rnastructurepath = '/usr/local/RNAstructure/exe/'
+
+
+def ringmapper():
+    return ringmapperpath
+
+def structureanalysistools():
+    return structureanalysistoolspath
+
+def rnastructure():
+    return rnastructurepath
